d0a8c1f0aeae304dbe588f59d01724a9ecf18711
lta14 <tanner.lukas@gmail.com>
2019-01-09 13:15:17 +0100

    qr code now working

--- app/src/main/java/com/example/tanne/tantrum/MainActivity.java
+++ app/src/main/java/com/example/tanne/tantrum/MainActivity.java
@@ -1,22 +1,29 @@
 package com.example.tanne.tantrum;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.zxing.Result;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+public class MainActivity extends AppCompatActivity {
+    private TextView m_TextMessage;
+    private EditText m_UrlText;
+    private EditText m_TokenText;
 
-    private TextView mTextMessage;
-    private ZXingScannerView m_ScannerView;
+    //qr code scanner object
+    private IntentIntegrator qrScan;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -25,13 +32,13 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    m_TextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    m_TextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    m_TextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -43,30 +50,58 @@ public class MainActivity extends AppCompatActivity implements ZXingScannerView.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        m_TextMessage = (TextView) findViewById(R.id.message);
+
+        m_UrlText = (EditText) findViewById(R.id.urlText);
+        m_TokenText = (EditText) findViewById(R.id.tokenText);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        //intializing scan object
+        qrScan = new IntentIntegrator(this);
     }
+
+    //Getting the scan results
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null)
+        {
+            //{"url":"https://apps.egiz.gv.at/cryptobinding/","token":561vr85pvv2famg9lec1pfg5do"}
+            
+            //if qrcode has nothing in it
+            if (result.getContents() == null)
+            {
+                Toast.makeText(this, "Result Not Found", Toast.LENGTH_LONG).show();
+            } else
+            {
+                //if qr contains data
+                try {
+                    //converting the data to json
+                    JSONObject obj = new JSONObject(result.getContents());
+                    //setting values to textviews
+                    m_UrlText.setText(obj.getString("url"));
+                    m_TokenText.setText(obj.getString("token"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    //if control comes here
+                    //that means the encoded format not matches
+                    //in this case you can display whatever data is available on the qrcode
+                    //to a toast
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        else
+        {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
 
     public void scan(View view) {
 
-        m_ScannerView = new ZXingScannerView(getApplicationContext());
-        setContentView(m_ScannerView);
-        m_ScannerView.setResultHandler(this);
-        m_ScannerView.startCamera();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        m_ScannerView.stopCamera();
-    }
-
-    @Override
-    public void handleResult(Result result) {
-        Toast.makeText(getApplicationContext(), result.getText(), Toast.LENGTH_SHORT).show();
-       // m_ScannerView.resumeCameraPreview(this);
-        setContentView(R.layout.activity_main);
-
+        qrScan.initiateScan();
     }
 }
